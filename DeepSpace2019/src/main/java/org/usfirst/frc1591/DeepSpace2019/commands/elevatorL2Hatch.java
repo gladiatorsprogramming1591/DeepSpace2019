736624dfa31fd6b89bb5c2cf4af22232d7395a17
dylantompkins <dylanltompkins@gmail.com>
2019-02-17 09:44:13 -0500

    convert elevator to encoder

---
+++ DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/elevatorL2Hatch.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc1591.DeepSpace2019.commands;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc1591.DeepSpace2019.Robot;
+
+public class elevatorL2Hatch extends Command {
+
+    int posIndex = 2;
+
+    public elevatorL2Hatch() {
+        requires(Robot.elevator);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+        Robot.elevator.moveInit(posIndex);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return Robot.elevator.moveIsFinished(posIndex);
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        Robot.elevator.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
