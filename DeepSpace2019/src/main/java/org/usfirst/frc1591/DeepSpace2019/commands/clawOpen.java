71d9851fd3d653b07cd958495499a21e9354d24e
Cooper559024 <mcmurray.aidan.c@gmail.com>
2019-02-17 16:24:10 -0500

    fixed claw commands.

--- DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/clawOpen.java
+++ DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/clawOpen.java
@@ -38,18 +38,20 @@ public class clawOpen extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        setTimeout(1);
+        Robot.claw.openClaw();
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        Robot.claw.openClaw();
+      
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
