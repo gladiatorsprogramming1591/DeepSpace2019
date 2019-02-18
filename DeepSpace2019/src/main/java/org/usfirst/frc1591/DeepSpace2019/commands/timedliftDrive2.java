07a12f3b323d1237e3ab54110061581710d77f2a
gladiatorsprogramming1591 <gladiatorsprogramming1591@gmail.com>
2019-02-18 17:33:39 -0500

    Added new liftFrontAndDrive command

--- DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/timedliftDrive2.java
+++ DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/timedliftDrive2.java
@@ -2,16 +2,16 @@ package org.usfirst.frc1591.DeepSpace2019.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc1591.DeepSpace2019.Robot;
 
-public class timedliftDrive extends Command {
+public class timedliftDrive2 extends Command {
     double m_timeout = 2.0;
     double m_speed = 0.65;
     
-    public timedliftDrive(double time) {
+    public timedliftDrive2(double time) {
         requires(Robot.lift);
         m_timeout = time;
     }
 
-    public timedliftDrive(double time, double speed) {
+    public timedliftDrive2(double time, double speed) {
         requires(Robot.lift);
         m_timeout = time;
     }
@@ -19,7 +19,7 @@ public class timedliftDrive extends Command {
     @Override
     protected void initialize() {
         setTimeout(m_timeout);
-        Robot.lift.turnWheel(m_speed);
+        Robot.lift.liftFrontAndDrive(m_speed);
     }
 
     @Override
