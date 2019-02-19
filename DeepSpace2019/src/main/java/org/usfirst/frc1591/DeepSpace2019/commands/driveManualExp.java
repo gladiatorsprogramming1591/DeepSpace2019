51c221edef27722cf688cada65430d2de56b8de9
Dylan Tompkins <36244177+dylantompkins@users.noreply.github.com>
2019-02-18 21:40:43 -0500

    added support for rocket angles

--- DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/driveManualExp.java
+++ DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/driveManualExp.java
@@ -8,6 +8,8 @@ public class driveManualExp extends Command {
     double targetAngle = 0;
     double currentPOV = 0;
 
+    boolean rocketAnglesMode = false;
+
     public driveManualExp() {
         targetAngle = 0;
         requires(Robot.driveTrain);
@@ -27,15 +29,47 @@ public class driveManualExp extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        final int RESOLUTION = 45;
+
+        if (Robot.oi.driveStick.getRawButtonPressed(2)) {
+            rocketAnglesMode = false;
+        }
+        else if (Robot.oi.driveStick.getRawButtonPressed(3)) {
+            rocketAnglesMode = true;
+        }
+
         if (!Robot.lift.checkPneumatics()) {
             double strafe = Robot.oi.driveStick.getRawAxis(2);
             double vertical = -Robot.oi.driveStick.getRawAxis(3);
             double gyroDeg = Robot.AHRS.getAngle();
+            int targetIndex = Robot.oi.driveStick.getPOV() / RESOLUTION;
+            boolean setTargetAngle = false;
             
-            if (Robot.oi.driveStick.getPOV() != currentPOV && Robot.oi.driveStick.getPOV()!= -1){
-                int targetIndex = Robot.oi.driveStick.getPOV() / 45;
+            if (rocketAnglesMode == false) {
+                switch (targetIndex) {
+                    case 0:
+                    case 2:
+                    case 4:
+                    case 6:
+                        setTargetAngle = true;
+                        break;
+                }
+            }
+            else if(rocketAnglesMode == true) {
+                switch (targetIndex) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                        setTargetAngle = true;
+                        break;
+                }
+            }
+            
+            if (setTargetAngle) {
                 targetAngle = Robot.driveTrain.Angles.get(targetIndex);
-                currentPOV = targetIndex * 45;
+                currentPOV = targetIndex * RESOLUTION;
+                System.out.println("Rocket angles mode: " + rocketAnglesMode + " Setting target index to " + targetIndex);
             }
         
             Robot.driveTrain.rotateToPos(strafe, vertical, gyroDeg, targetAngle);
