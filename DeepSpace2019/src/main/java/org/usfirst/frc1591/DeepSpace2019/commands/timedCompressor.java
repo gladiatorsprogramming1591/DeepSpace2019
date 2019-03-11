952bdc527086750197e61099a9050388e6feef04
Kenan Celik <ckenan1337@gmail.com>
2019-03-11 18:58:39 -0400

    added button on manpulator stick for compressor

---
+++ DeepSpace2019/src/main/java/org/usfirst/frc1591/DeepSpace2019/commands/timedCompressor.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc1591.DeepSpace2019.commands;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc1591.DeepSpace2019.Robot;
+
+public class timedCompressor extends Command {
+
+    
+    public timedCompressor() {
+        requires(Robot.compressorSS);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        Robot.compressorSS.startCompressor();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        Robot.compressorSS.stopCompressor();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
